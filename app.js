16b63301c94d6fe36dbeadadebe30a7540bc7cfb
Through2Dexter <162742084+Through2Dexter@users.noreply.github.com>
2025-02-19 21:32:34 +0000

    glow effect on grid

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ document.addEventListener("DOMContentLoaded", (e) => {
     splash.classList.add("display-none");
   }, 2000);
 });
+
+const handleOnMouseMove = e => {
+  const {currentTarget: target} = e 
+  const rect = target.getBoundingClientRect(),
+  x=e.clientX - rect.left,
+  y = e.clientY - rect.top;
+  target.style.setProperty('--mouse-x', `${x}px`);
+}
+
+
+for(const card of document.querySelectorAll('.card')) {
+  card.onmousemove = e => handleOnMouseMove(e)
+}
